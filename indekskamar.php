d715879b3590b3c29277cf1a88ddd756ed2cf594
Wahyu & Furina hehehe <161020966+KamisatoWahyu@users.noreply.github.com>
2024-05-28 00:40:46 +0700

    Update indekskamar.php

--- indekskamar.php
+++ indekskamar.php
@@ -1,105 +1,17 @@
 <?php
-require 'function.php';
-require 'cek.php';
-
+    include 'lib/header.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Data Kamar</title>
-        <link href="css/styles.css" rel="stylesheet" />
-        <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
-    </head>
-    <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <a class="navbar-brand" href="index.html">Catatan Kost</a>
-            <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
-            <!-- Navbar Search-->
-            <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-                <div class="input-group">
-                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
-                    <div class="input-group-append">
-                        <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
-                    </div>
-                </div>
-            </form>
-            <!-- Navbar-->
-            
-        </nav>
-        <div id="layoutSidenav">
-            <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <?php
+
+                <main>
+                    <div class="container-fluid">
+                    <br>
+                    <?php
                             $emailadmin = $_SESSION['username'];
                             $ambiladmin = mysqli_query($conn, "SELECT lvl, Nama_Admin as namaadmin FROM login Where Email = '$emailadmin'");
                             $namaadmin = mysqli_fetch_array($ambiladmin);
                             $ambilnamanya = $namaadmin['namaadmin'];
                             $lvladmin = $namaadmin['lvl'];
-                            ?>
-
-                            <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="index.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Halaman Utama
-                            </a>
-                            <a class="nav-link" href="indekspenyewa.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                Penyewa
-                            </a>
-                            <a class="nav-link" href="indekskamar.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                Kamar
-                            </a>
-                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Laporan Keuangan
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="indekstagihan.php">Laporan Tagihan</a>
-                                    <a class="nav-link" href="indekslaporan.php">Laporan Transaksi</a>
-                                    <a class="nav-link" href="indeksrekaman.php">Rekaman Transaksi</a>
-                                </nav>
-                            </div>
-                            <div class="sb-sidenav-menu-heading">Lainnya</div>
-                            <?php 
-                                if($lvladmin == 'owner'){
-                            ?>
-                            <a class="nav-link" href="indeksadmin.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                                Kelola Admin
-                            </a>
-                            <?php } ?>
-                            <a class="nav-link" data-toggle="modal" data-target="#logoutmodal">
-                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                                Logout
-                            </a>
-                            
-                        </div>
-                    </div>
-                    <div class="sb-sidenav-footer">
-                        
-                        <div class="small">
-                            Logged in as:
-                            <br><?=$ambilnamanya;?>
-                        </div>
-                        
-                    </div>
-                </nav>
-            </div>
-            <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid">
-                    <br>
+                    ?>
                         <?php
                             $ambillaporan=mysqli_query($conn,"SELECT * from data_kamar where status='Kosong'");
                             $ceklaporan=mysqli_num_rows($ambillaporan);
@@ -119,7 +31,7 @@ require 'cek.php';
                             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
                                  Tambah Kamar
                             </button>
-                            <a href = "exportkamar.php" class="btn btn-info" target="_blank">
+                            <a href = "export/exportkamar.php" class="btn btn-info" target="_blank">
                                  Export data
                             </a>
                             </div>
@@ -190,8 +102,7 @@ require 'cek.php';
                                                                 <br>
                                                                 <input type="text" name="jumlahfasilitas" value="<?=$jumlahfasilitas;?>" class="form-control" required>
                                                                 <br>
-                                                                <input type="text" name="biaya" value="<?=$biaya;?>" class="form-control" <?php if($lvladmin=='
-                                                                ') { ?> required <?php }else{ ?> readonly <?php } ?>>
+                                                                <input type="text" name="biaya" value="<?=$biaya;?>" class="form-control" <?php if($lvladmin=='owner') { ?> required <?php }else{ ?> readonly <?php } ?>>
                                                                 <br>
                                                                 <input type="hidden" name="idkamar" value="<?=$idkamar;?>">
                                                                 <button type="submit" class="btn btn-warning" name="updatekamar">Edit</button>
@@ -263,30 +174,6 @@ require 'cek.php';
                         </div>
                     </div>
                 </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Inovasi SI dan New Technology | Wahyu Hariyadi Nurdin | 11121286</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
-        </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/chart-area-demo.js"></script>
-        <script src="assets/demo/chart-bar-demo.js"></script>
-        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/datatables-demo.js"></script>
-    </body>
 
     <!-- The Modal -->
     <div class="modal fade" id="myModal">
@@ -314,4 +201,6 @@ require 'cek.php';
                             </div>
                         </form> 
 
-</html>
+<?php
+    include 'lib/footer.php';
+?>
