2069d9781c59b7a395b1ecdc37b50591ba81baf5
Wahyu & Furina hehehe <161020966+KamisatoWahyu@users.noreply.github.com>
2024-05-28 00:40:31 +0700

    Create header.php

---
+++ lib/header.php
@@ -0,0 +1,92 @@
+<?php
+require 'function.php';
+require 'cek.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Kelola Admin</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.html">Catatan Kost</a>
+            <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+                <div class="input-group">
+                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
+                    </div>
+                </div>
+            </form>
+            <!-- Navbar-->
+            
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="index.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Halaman Utama
+                            </a>
+                            <a class="nav-link" href="indekspenyewa.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                                Penyewa
+                            </a>
+                            <a class="nav-link" href="indekskamar.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                                Kamar
+                            </a>
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Laporan Keuangan
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="indekstagihan.php">Laporan Tagihan</a>
+                                    <a class="nav-link" href="indekslaporan.php">Laporan Transaksi</a>
+                                    <a class="nav-link" href="indeksrekaman.php">Rekaman Transaksi</a>
+                                </nav>
+                            </div>
+                            <div class="sb-sidenav-menu-heading">Lainnya</div>
+                            <a class="nav-link" href="indeksadmin.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                                Kelola Admin
+                            </a>
+                            <a class="nav-link" data-toggle="modal" data-target="#logoutmodal">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Logout
+                            </a>
+                            
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <?php
+                        $emailadmin = $_SESSION['username'];
+                        $ambiladmin = mysqli_query($conn, "SELECT Nama_Admin as namaadmin FROM login Where Email = '$emailadmin'");
+                        $namaadmin = mysqli_fetch_array($ambiladmin);
+                        $ambilnamanya = $namaadmin['namaadmin'];
+                        ?>
+
+                        <div class="small">
+                            Logged in as:
+                            <br><?=$ambilnamanya;?>
+                        </div>
+                        
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
